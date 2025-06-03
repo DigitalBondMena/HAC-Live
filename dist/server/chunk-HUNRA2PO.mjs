70b07efd9516e2bf7038851380e0173a277fc93e
marwanabubakr <marwanabubakr0o.o0@gmail.com>
2025-06-03 17:29:54 +0300

    run csr

--- dist/server/chunk-HUNRA2PO.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{g as u}from"./chunk-JZUHZIDU.mjs";import"./chunk-67BHZS7I.mjs";import{Mb as a,jb as o}from"./chunk-DD3AZI3M.mjs";import"./chunk-X2SEQXRR.mjs";var n=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-auth-layout"]],decls:1,vars:0,template:function(t,i){t&1&&a(0,"router-outlet")},dependencies:[u],encapsulation:2})};export{n as AuthLayout};
