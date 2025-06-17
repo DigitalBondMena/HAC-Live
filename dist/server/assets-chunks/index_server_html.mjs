dd9e955ef34b9ca07bc3cdbbd6679172353094a9
marwanabubakr <marwanabubakr0o.o0@gmail.com>
2025-06-17 18:45:34 +0300

    add search console in html

--- dist/server/assets-chunks/index_server_html.mjs
+++ dist/server/assets-chunks/index_server_html.mjs
@@ -3,6 +3,7 @@ export default `<!DOCTYPE html>
   <head>
     <meta charset="utf-8"/>
     <title>HAC</title>
+    <meta name="google-site-verification" content="MB5P51CU7W3g2K3w6uJETtdy3QZzU2XGjMGEdRcZHJ8"/>
     <link rel="canonical" href="https://haccosmetics.com/ar/"/>
     <base href="/"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -53,6 +54,6 @@ export default `<!DOCTYPE html>
   <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script>
     <app-root></app-root>
-  <link rel="modulepreload" href="chunk-CAWKRA22.js"><link rel="modulepreload" href="chunk-LX6HB4B4.js"><link rel="modulepreload" href="chunk-Z2WD4PVM.js"><link rel="modulepreload" href="chunk-LSU73Y2J.js"><link rel="modulepreload" href="chunk-QJQKHYAI.js"><link rel="modulepreload" href="chunk-EKQX75D2.js"><link rel="modulepreload" href="chunk-4OK5BLJZ.js"><link rel="modulepreload" href="chunk-S7VUZIXW.js"><link rel="modulepreload" href="chunk-W2LJ2ZLD.js"><link rel="modulepreload" href="chunk-SKDDIVFS.js"><script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-JHDLLA36.js" type="module"></script></body>
+  <link rel="modulepreload" href="chunk-CAWKRA22.js"><link rel="modulepreload" href="chunk-3UVK4OFB.js"><link rel="modulepreload" href="chunk-456WIVJJ.js"><link rel="modulepreload" href="chunk-LSU73Y2J.js"><link rel="modulepreload" href="chunk-QJQKHYAI.js"><link rel="modulepreload" href="chunk-BD57PW3G.js"><link rel="modulepreload" href="chunk-MBC5ZSEN.js"><link rel="modulepreload" href="chunk-CYIGHDJZ.js"><link rel="modulepreload" href="chunk-W2LJ2ZLD.js"><link rel="modulepreload" href="chunk-SKDDIVFS.js"><script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-YAUJXSIV.js" type="module"></script></body>
 </html>
 `;
