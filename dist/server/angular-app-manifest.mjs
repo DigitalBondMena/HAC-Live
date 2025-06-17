1238225cec130bd6851445d44a5d8f854408ef21
marwanabubakr <marwanabubakr0o.o0@gmail.com>
2025-06-17 11:20:03 +0300

    update sitemaps

--- dist/server/angular-app-manifest.mjs
+++ dist/server/angular-app-manifest.mjs
@@ -238,8 +238,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 42886, hash: '2d3008a9cdead81678fff039124e01347bbedb34f07d5c27629332fc3573f0c9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3821, hash: 'b1bc14a8caa00788eac2594cc2bc86e34606461695ea82fbec9449b1040c88f2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 42886, hash: '10f150774f1a847f3cc87f264d795d481f62bf4b4e240bbac42e818e1e00cbbd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3821, hash: 'bd081ec7634640371a3b553f236b620b83ef2680f259e4fb371418e869f73e1d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-VQDIA34F.css': {size: 234343, hash: 'bIicCRQTBS4', text: () => import('./assets-chunks/styles-VQDIA34F_css.mjs').then(m => m.default)}
   },
 };
