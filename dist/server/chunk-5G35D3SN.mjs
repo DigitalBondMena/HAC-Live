2922de5ac90f2de9b425658abb456f34eb794ef0
marwanabubakr <marwanabubakr0o.o0@gmail.com>
2025-08-21 19:51:15 +0300

    redeploy

--- dist/server/chunk-5G35D3SN.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a as e,b as i}from"./chunk-SAQVWPEB.mjs";import{T as t,Z as r}from"./chunk-MASI3ZRX.mjs";var a=class o{apiService=r(i);constructor(){}getAboutData(){return console.log("getAboutData"),this.apiService.get(e.STATIC_PAGES.ABOUT_US)}static \u0275fac=function(c){return new(c||o)};static \u0275prov=t({token:o,factory:o.\u0275fac,providedIn:"root"})};export{a};
--- dist/server/chunk-HXFUATEV.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-77GQZD64.mjs";import"./chunk-MASI3ZRX.mjs";import"./chunk-X2SEQXRR.mjs";export{a as BannerComponent};
